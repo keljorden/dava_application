c4fe370b421ad04cfb359e42a518fbbc1c0ca1b6
keljor <keljor11afly@gmail.com>
2026-07-14 18:18:59 +0530

    this .py file contains wrapper function for widgets such as Button, Frame, etc...

---
+++ layout.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import ttk
+
+# split windows vertically or horizontally 
+def split_window(root: str, dir: str = 'HORIZONTAL'):
+    if dir == 'HORIZONTAL':
+        split = ttk.PanedWindow(root, orient= tk.HORIZONTAL)
+    elif dir == 'VERTICAL':
+        split = ttk.PanedWindow(root, orient= tk.VERTICAL)
+    else: return 'Unsupported split attempted'
+    return split
+
+# create frame to add to the window
+def create_frame(window: str, **kwargs):
+    new_frame = ttk.Frame(window, **kwargs)
+    return new_frame
+
+# add frame to the parent window/frame
+def add_frame(parent_frame: str, child_frame: str, w: int = 1):
+    parent_frame.add(child_frame, weight= w)
+
+
+def add_lable(frame: str, text: str, font =("Arial", 10, "bold"), **kwargs):
+    lbl = ttk.Label(frame, text = text, font = font, **kwargs)
+    return lbl
+
+def add_button(frame: str, text: str, command = None, **kwargs):
+    kwargs.setdefault("cursor", "hand2")
+    btn = ttk.Button(frame, text = text, command = command, **kwargs)
+    return btn
+
+def add_combobox(frame, values: list, textvariable: str, **kwargs):
+    kwargs.setdefault("state", "readonly")
+    cb = ttk.Combobox(frame, values=values, textvariable = textvariable, **kwargs)
+    cb.current(0)
+    return cb
