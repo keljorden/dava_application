1a31012c5f14fc3af3b12d9a96f48689ad496cf0
keljor <keljor11afly@gmail.com>
2026-07-16 12:59:36 +0530

    added features to view data descriptions

--- dava.py
+++ dava.py
@@ -13,7 +13,7 @@ plot_type = ['scatter plot', 'line plot','hist plot',
 # 1. Initialize the main application window
 root = tk.Tk()
 root.title("Dava")
-root.geometry("1024x512")
+root.geometry("1024x720")
 
 selected_plot = tk.StringVar(value=plot_type[0])
 
@@ -31,37 +31,45 @@ add_frame(main_paned,right_frame, 2)
 
 # 5. Create Top, Mid and Bottom frames inside the Left PanedWindow
 
-top_left_frame = create_frame(left_paned, relief=tk.SUNKEN, height=150)
+top_left_paned = split_window(main_paned, 'VERTICAL')
+add_frame(left_paned,top_left_paned, 1)
+
 mid_left_frame  = create_frame(left_paned, relief=tk.SUNKEN, height=150)
 bottom_left_frame = create_frame(left_paned, relief=tk.SUNKEN, height=250)
 
+top_left_file_control_frame = create_frame(top_left_paned, relief=tk.SUNKEN, height=80)
+top_left_data_desc_frame = create_frame(top_left_paned, relief=tk.SUNKEN, height=150)
+
 #Add them to the left vertical split
-add_frame(left_paned,top_left_frame)
+add_frame(top_left_paned, top_left_file_control_frame, 0)
+add_frame(top_left_paned, top_left_data_desc_frame, 2)
 add_frame(left_paned,mid_left_frame)
 add_frame(left_paned,bottom_left_frame)
 
-#Top-Left Partition
+#top left file control frame
 
 # Configure Column Weights (Ratio 1 : 3) 
-top_left_frame.grid_columnconfigure(1, weight=1)
-top_left_frame.grid_columnconfigure(0, weight=3)
+top_left_file_control_frame.grid_columnconfigure(0, weight=3)
+top_left_file_control_frame.grid_columnconfigure(1, weight=1)
 
-add_lable(top_left_frame, text="File Controls").grid( row=0, column=0, columnspan=2, pady=5)
+add_lable(top_left_file_control_frame, text="File Controls").grid( row=0, column=0, columnspan=2, pady=5)
 
-data_load_btn = add_button(top_left_frame, text="Add Data", command=lambda: on_click_add_data(mid_left_frame))
+data_load_btn = add_button(top_left_file_control_frame, text="Add Data", command=lambda: on_click_add_data(mid_left_frame, top_left_data_desc_frame))
 data_load_btn.grid(row=1, column=0, columnspan=2, sticky="ew", padx=10, pady=2)
 
-visualize_btn = add_button(top_left_frame, text="Visualize")
+visualize_btn = add_button(top_left_file_control_frame, text="Visualize")
 visualize_btn.grid(row=2, column=0, columnspan=2, sticky="ew", padx=10, pady=2)
 
-plot_type_cb = add_combobox(top_left_frame, values=plot_type, textvariable=selected_plot )
+plot_type_cb = add_combobox(top_left_file_control_frame, values=plot_type, textvariable=selected_plot )
 plot_type_cb.grid(row=3, column=0, sticky="ew", padx=10, pady=2)
 
-select_plot_btn = add_button(top_left_frame, text="Select", command=lambda: on_click_select(bottom_left_frame, selected_plot))
+select_plot_btn = add_button(top_left_file_control_frame, text="Select", command=lambda: on_click_select(bottom_left_frame, selected_plot))
 select_plot_btn.grid( row=3, column=1, sticky="ew", padx=(2,10), pady=2 )
 
+#top_left_data_desc_frame
+add_lable(top_left_data_desc_frame, text="Dataset Properties").pack(pady=5)
 #mid-left partition
-add_lable(mid_left_frame, text="Data Preview", font=("Arial", 10, "bold")).pack(pady=(10, 5), padx=10, anchor=tk.W)
+add_lable(mid_left_frame, text="Dataset Preview", font=("Arial", 10, "bold")).pack(pady=5)
 
 #Populate Bottom-Left Partition
 add_lable(bottom_left_frame, text="Plot Controls").pack(pady=5)
