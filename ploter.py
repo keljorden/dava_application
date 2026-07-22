0108fe4640c32ea37b5a056d2d727f0c336d5a76
keljor <keljor11afly@gmail.com>
2026-07-22 15:55:13 +0530

    added code for displot and catplot.

--- ploter.py
+++ ploter.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import matplotlib.pyplot as plt
+from tkinter import messagebox
 import seaborn as sns
 from wrapper import *
 
@@ -29,6 +30,9 @@ class PlotBuilder:
 
         self.clean_x = None
         self.clean_y = None
+        self.clean_hue = None
+        self.plt = None
+        self.err = None
 
         self.build_ui()
 
@@ -38,7 +42,8 @@ class PlotBuilder:
         method_name = self._plot_map[self.kind]
         plot_method = getattr(self, method_name)
         
-        self.fig = plot_method(**kwargs)
+        self.plt, self.err = plot_method(**kwargs)
+        
 
     def update_column_info(self, event=None):
         if self.data is None:
@@ -46,10 +51,12 @@ class PlotBuilder:
             
         x_col = self.x_var.get()
         y_col = self.y_var.get()
+        hue_col = self.hue_var.get()
 
         if event is not None:
             self.clean_x = None
             self.clean_y = None
+            self.clean_hue = None
 
         if x_col in self.data.columns:
             if self.clean_x is not None and self.clean_x.name == x_col:
@@ -73,6 +80,22 @@ class PlotBuilder:
             self.y_na_entry.insert(0, str(y_nas))
             self.y_na_entry.config(state='readonly')
 
+        if hue_col and hue_col != 'None' and hue_col in self.data.columns:
+            if self.clean_hue is not None and self.clean_hue.name == hue_col:
+                hue_nas = self.clean_hue.isna().sum()
+            else:
+                hue_nas = self.data[hue_col].isna().sum()
+            
+            hue_val = str(hue_nas)
+        else:
+            hue_val = "-"  
+
+        self.hue_na_entry.config(state='normal')
+        self.hue_na_entry.delete(0, tk.END)
+        self.hue_na_entry.insert(0, hue_val)
+        self.hue_na_entry.config(state='readonly')
+
+
     def _ploter_relplot(self, **kwargs):
         
         try:
@@ -80,17 +103,21 @@ class PlotBuilder:
             y_name = self.y_var.get()
             hue_name = self.hue_var.get()
 
-            if x_name == "None" or y_name == "None":
+            if not x_name or not y_name:
+                messagebox.showerror("Missing value error: ", " X or Y is not selected or mising")
                 return None
 
             plot_x = self.clean_x if (self.clean_x is not None and self.clean_x.name == x_name) else self.data[x_name]
             plot_y = self.clean_y if (self.clean_y is not None and self.clean_y.name == y_name) else self.data[y_name]
             
             if len(plot_x) != len(plot_y):
-                print("Error: X and Y data lengths do not match.")
+                messagebox.showerror("Length error: ", " X and Y have different length")
                 return None
 
-            hue_data = self.data[hue_name] if hue_name != "None" else None
+            if hue_name and hue_name != 'None' and hue_name in self.data.columns:
+                hue_data = self.clean_hue if (self.clean_hue is not None and self.clean_hue.name == hue_name) else self.data[hue_name]
+            else:
+                hue_data = None
 
             grid = sns.relplot(
                 x=plot_x, 
@@ -100,27 +127,71 @@ class PlotBuilder:
                 **kwargs
             )
             grid.set_axis_labels(x_name, y_name)
-            return grid.fig
+            return grid.fig, None
 
         except Exception as e:
-
-            import traceback
-            traceback.print_exc() 
-            failed_lable = add_lable(frame=self.frame, text=f"Plot Error: {e}")
-            failed_lable.grid(row=8, column=0, columnspan=3, sticky='ew', padx=10, pady=10)
-            return None
+            return None, e
         
     def _ploter_displot(self, **kwargs):
-        grid = sns.displot(data=self.data, kind=self.kind, **kwargs)
-        return grid.fig
+        try:
+            x_name = self.x_var.get()
+            hue_name = self.hue_var.get()
+
+            if not x_name:
+                messagebox.showerror("Missing value error: ", " X is not selected or mising")
+                return None
+
+            plot_x = self.clean_x if (self.clean_x is not None and self.clean_x.name == x_name) else self.data[x_name]
+
+            if hue_name and hue_name != 'None' and hue_name in self.data.columns:
+                hue_data = self.clean_hue if (self.clean_hue is not None and self.clean_hue.name == hue_name) else self.data[hue_name]
+            else:
+                hue_data = None
+    
+            grid = sns.displot(
+                x=plot_x, 
+                hue=hue_data, 
+                kind=self.kind, 
+                **kwargs
+            )
+            return grid.fig, None
+        except Exception as e:
+            return None, e
 
     def _ploter_catplot(self, **kwargs):
-        grid = sns.catplot(data=self.data, kind=self.kind, **kwargs)
-        return grid.fig
-    
+        try:
+            x_name = self.x_var.get()
+            y_name = self.y_var.get()
+            hue_name = self.hue_var.get()
 
+            if not x_name or not y_name:
+                messagebox.showerror("Missing value error: ", " X or Y is not selected or mising")
+                return None
 
+            plot_x = self.clean_x if (self.clean_x is not None and self.clean_x.name == x_name) else self.data[x_name]
+            plot_y = self.clean_y if (self.clean_y is not None and self.clean_y.name == y_name) else self.data[y_name]
+            
+            if len(plot_x) != len(plot_y):
+                messagebox.showerror("Length error: ", " X and Y have different length")
+                return None
 
+            if hue_name and hue_name != 'None' and hue_name in self.data.columns:
+                hue_data = self.clean_hue if (self.clean_hue is not None and self.clean_hue.name == hue_name) else self.data[hue_name]
+            else:
+                hue_data = None
+
+            grid = sns.catplot(
+                x=plot_x, 
+                y=plot_y, 
+                kind=self.kind, 
+                hue=hue_data, 
+                **kwargs
+            )
+            grid.set_axis_labels(x_name, y_name)
+            return grid.fig, None
+
+        except Exception as e:
+            return None, e
 
 
     def build_ui(self):
@@ -129,10 +200,14 @@ class PlotBuilder:
             
         self.frame.grid_columnconfigure(1, weight=2)
 
-        cols = ['None'] + list(self.data.columns)
-        self.x_var = tk.StringVar(value=cols[1])
-        self.y_var = tk.StringVar(value=cols[2] if len(cols) > 1 else cols[1])
-        self.hue_var = tk.StringVar(value=cols[0])
+        cols = list(self.data.columns)
+        hue_choice = ['None'] + cols
+        self.x_var = tk.StringVar(value=cols[0])
+        self.y_var = tk.StringVar(value=cols[1] if len(cols) > 1 else cols[0])
+        self.hue_var = tk.StringVar(value=hue_choice[0])
+
+        plot_type_lable = add_lable(frame = self.frame, text = 'Plot Type:')
+        plot_type = add_lable(frame = self.frame, text = f'{self.kind}' + 'plot')
 
         x_lable = add_lable(frame = self.frame, text = 'X-axis:')
         y_lable = add_lable(frame = self.frame, text = 'Y-axis:')
@@ -146,6 +221,12 @@ class PlotBuilder:
         self.x_na_entry = add_entry(frame = self.frame, text = '0')
         self.y_na_entry = add_entry(frame = self.frame, text = '0')
 
+        hue_lable = add_lable(frame = self.frame, text = 'Hue:')
+        hue_combo = add_combobox(frame = self.frame, textvariable = self.hue_var, values = cols)
+
+        hue_na = add_lable(frame=self.frame, text = 'Hue N/A Count:')
+        self.hue_na_entry = add_entry(frame = self.frame, text = '0')
+
         clean_lable = add_lable(frame = self.frame, text = 'N/A strategies:')
         clean_combo = add_combobox(frame = self.frame, textvariable = self.data_cleaner_approach, values=self._na_strategies)
 
@@ -155,43 +236,44 @@ class PlotBuilder:
             command = lambda: on_click_clean_btn(
                 plot_instance = self,
                 clean_strategie=self.data_cleaner_approach.get(),
-                xcol=self.data[self.x_var.get()].copy() if (self.x_var.get() != 'None' and self.x_var.get()) else None,
-                ycol=self.data[self.y_var.get()].copy() if (self.y_var.get() != 'None' and self.y_var.get()) else None
+                xcol=self.data[self.x_var.get()].copy() if self.x_var.get() else None,
+                ycol=self.data[self.y_var.get()].copy() if self.y_var.get() else None,
+                hue=self.data[self.hue_var.get()].copy() if (self.hue_var.get() and self.hue_var.get() != "None" and self.hue_var.get() in self.data.columns) else None
             )
         )
 
-        hue_lable = add_lable(frame = self.frame, text = 'Hue:')
-        hue_combo = add_combobox(frame = self.frame, textvariable = self.hue_var, values = cols)
+        plot_type_lable.grid(row = 0, column = 0, sticky= 'ew', padx = 10, pady = 10)
+        plot_type.grid(row = 0, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 10)
 
-        download = add_button(frame = self.frame, text = 'Download as PDF', command = lambda: on_click_download)
+        x_lable.grid(row = 1, column = 0, sticky= 'ew', padx = 10, pady = 5)
+        x_combo.grid(row = 1, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
 
-        x_lable.grid(row = 0, column = 0, sticky= 'ew', padx = 10, pady = 5)
-        x_combo.grid(row = 0, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
-
-        x_na.grid(row = 1, column = 0, sticky = 'ew', padx = 10, pady = 5)
-        self.x_na_entry.grid(row = 1, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
+        x_na.grid(row = 2, column = 0, sticky = 'ew', padx = 10, pady = 5)
+        self.x_na_entry.grid(row = 2, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
 
         if self.kind != 'hist' and self.kind != 'kde' and self.kind != 'ecdf':
-            y_lable.grid(row = 2, column = 0, sticky = 'ew', padx = 10, pady = 5)
-            y_combo.grid(row = 2, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
+            y_lable.grid(row = 3, column = 0, sticky = 'ew', padx = 10, pady = 5)
+            y_combo.grid(row = 3, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
 
-            y_na.grid(row = 3, column = 0, sticky = 'ew', padx = 10, pady = 5)
-            self.y_na_entry.grid(row = 3, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
+            y_na.grid(row = 4, column = 0, sticky = 'ew', padx = 10, pady = 5)
+            self.y_na_entry.grid(row = 4, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
 
-        clean_lable.grid(row = 4, column = 0, sticky= 'ew', padx = 10, pady = 5)
-        clean_combo.grid(row = 4, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
+        hue_lable.grid(row = 5, column=0, columnspan=1, sticky= 'ew', padx= 10, pady = 5)
+        hue_combo.grid(row = 5, column=1, columnspan=2, sticky= 'ew', padx= 10, pady = 5)
 
-        clean_btn.grid(row = 5, column=0, columnspan=4, sticky= 'ew', padx= 10, pady = 5)
+        hue_na.grid(row = 6, column=0, columnspan=1, sticky= 'ew', padx= 10, pady = 5)
+        self.hue_na_entry.grid(row = 6, column=1, columnspan=2, sticky= 'ew', padx= 10, pady = 5)
 
-        hue_lable.grid(row = 6, column=0, columnspan=1, sticky= 'ew', padx= 10, pady = 5)
-        hue_combo.grid(row = 6, column=1, columnspan=2, sticky= 'ew', padx= 10, pady = 5)
+        clean_lable.grid(row = 7, column = 0, sticky= 'ew', padx = 10, pady = 5)
+        clean_combo.grid(row = 7, column = 1, columnspan = 2, sticky = 'ew', padx = 10, pady = 5)
 
-        download.grid(row = 7, column=0, columnspan=3, sticky= 'ew', padx= 10, pady = 10)
+        clean_btn.grid(row = 8, column=0, columnspan=3, sticky= 'ew', padx= 10, pady = 5)
 
         self.x_combo = x_combo
         self.y_combo = y_combo
         
         x_combo.bind("<<ComboboxSelected>>", self.update_column_info)
         y_combo.bind("<<ComboboxSelected>>", self.update_column_info)
+        hue_combo.bind("<<ComboboxSelected>>", self.update_column_info)
 
         self.update_column_info()
